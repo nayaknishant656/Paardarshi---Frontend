abd27060682359e7fb99280140e9aff81d224f53
Nishant nayak <nayaknishant656@gmail.com>
2024-04-08 00:24:02 +0530

    change guys

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -5,8 +5,8 @@ export default class footer extends Component {
     return (
       <main>
         <div className='parent-div'>
-          <p>Beta Version 1.0 ( i know there are many bug to be fixed )</p>
-          <p>Design and Builded By <a href='https://bio.link/nishantkcu'>Nishant Nayak</a></p>
+          <p>Beta Version 1.0 ( i know there are many bug to be fixed )</p><br></br>
+          <p>Design and Builded By <a className='link' href='https://bio.link/nishantkcu'>Nishant Nayak</a></p><br></br>
           <p>i know there is no need of this product inspite i builded for my people</p><br></br>
           <p>and i also know majority will not going to use this product, i love building stuff</p>
           <p>so yeah, i dont need anything back from this and i am not here to prove something here</p>
