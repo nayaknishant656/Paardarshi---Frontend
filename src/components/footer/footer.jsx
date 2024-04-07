5cfdbe753e1367e696787a8f05074c9f25fd4ce0
Nishant nayak <nayaknishant656@gmail.com>
2024-04-08 00:05:07 +0530

    footer-change

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,16 @@ export default class footer extends Component {
   render() {
     return (
       <main>
-        FOOTER HUN GAANDU
+        <div className='parent-div'>
+          <p>Beta Version 1.0 ( i know there are many bug to be fixed )</p>
+          <p>Design and Builded By <a href='https://bio.link/nishantkcu'>Nishant Nayak</a></p>
+          <p>i know there is no need of this product inspite i builded for my people</p><br></br>
+          <p>and i also know majority will not going to use this product, i love building stuff</p>
+          <p>so yeah, i dont need anything back from this and i am not here to prove something here</p>
+          <p>i have been building stuff when i was 17 so this is no new to me</p>
+          <p>but it was fun project :) builded in 5 days</p><br></br>
+          <p>~nishant</p>
+        </div>
       </main>
     )
   }
