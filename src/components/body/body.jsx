098176730554c8edd6b491d6be9433745e9d869e
Nishant nayak <nayaknishant656@gmail.com>
2024-06-25 10:05:31 +0530

    risk

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -14,7 +14,7 @@ export default function Body() {
     // const [filterGenre, setFilterGenre] = useState([]);
     const [page, setPage] = useState(1);
     const [search, setSearch] = useState("");
-    const base_url = 'https://backend-chi-woad.vercel.app/shoes';
+    const base_url = 'https://backend-chi-woad.vercel.app/';
 
     useEffect(() => {
         const fetchData = async () => {
