25c629d6079b85bd3b6bec81c04b3389ea36b1ee
nayaknishant656 <nayaknishant656@gmail.com>
2026-01-31 20:30:24 +0530

    changed the hindi header to english header in the search funcationality

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,12 +1,11 @@
-import styles from "./styles.module.css";
 import { CiSearch } from "react-icons/ci";
 import "./search.css"
 
 const Search = ({ setSearch }) => {
-	return ( <>
+	return (<>
 		<input
 			className="search_bar"
-			placeholder="Search in HINDI **"
+			placeholder="Search the name of the donors**"
 			onChange={({ currentTarget: input }) => setSearch(input.value)}
 		/><CiSearch className="search_logo" />
 	</>
